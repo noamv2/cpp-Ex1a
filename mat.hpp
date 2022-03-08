35807c7e56ef9b9dc52a62d03c9ae7014ee80eaa
noamv2 <74304423+noamv2@users.noreply.github.com>
2022-03-08 14:57:41 +0200

    Update mat.hpp

--- mat.hpp
+++ mat.hpp
@@ -2,6 +2,13 @@
 #include <string>
 #include <algorithm>
 using std::string;
+
+/** *
+ * AUTHORS: <Noam Vanunu>
+ * 
+ * Date: 08/03/2022
+ */
+
 namespace ariel{
 
      string mat(int, int, char, char);
